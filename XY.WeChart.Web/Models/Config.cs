79994c69401bc2ceae8a5e93eb55392a715d69d0
yunchenglk <yunchenglk@163.com>
2016-03-11 00:09:59 +0800

    1

---
+++ XY.WeChart.Web/Models/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XY.WeChart.Web.Models
+{
+    public class Config
+    {
+        public int QrCodeId { get; set; }
+        public List<string> Versions { get; set; }
+        public int DownloadCount { get; set; }
+
+    }
+}
