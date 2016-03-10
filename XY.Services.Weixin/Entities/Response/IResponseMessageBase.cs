f9fe1406fa70373582d01e9830bdc4a9eab09899
yunchenglk <yunchenglk@163.com>
2016-03-10 23:59:32 +0800

    1

---
+++ XY.Services.Weixin/Entities/Response/IResponseMessageBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XY.Entity.Weixin;
+
+namespace XY.Services.Weixin.Entities
+{
+    public interface IResponseMessageBase : IMessageBase
+    {
+        ResponseMsgType MsgType { get; }
+        string Content { get; set; }
+        bool FuncFlag { get; set; }
+    }
+}
---
+++ XY.Services.Weixin/Helpers/JsonSetting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XY.Services.Weixin
+{
+    /// <summary>
+    /// JSON输出设置
+    /// </summary>
+    public class JsonSetting
+    {
+        /// <summary>
+        /// 是否忽略当前类型以及具有IJsonIgnoreNull接口，且为Null值的属性。如果为true，符合此条件的属性将不会出现在Json字符串中
+        /// </summary>
+        public bool IgnoreNulls { get; set; }
+        /// <summary>
+        /// 需要特殊忽略null值的属性名称
+        /// </summary>
+        public List<string> PropertiesToIgnore { get; set; }
+        /// <summary>
+        /// 指定类型（Class，非Interface）下的为null属性不生成到Json中
+        /// </summary>
+        public List<Type> TypesToIgnore { get; set; }
+
+
+        /// <summary>
+        /// JSON输出设置 构造函数
+        /// </summary>
+        /// <param name="ignoreNulls">是否忽略当前类型以及具有IJsonIgnoreNull接口，且为Null值的属性。如果为true，符合此条件的属性将不会出现在Json字符串中</param>
+        /// <param name="propertiesToIgnore">需要特殊忽略null值的属性名称</param>
+        /// <param name="typesToIgnore">指定类型（Class，非Interface）下的为null属性不生成到Json中</param>
+        public JsonSetting(bool ignoreNulls = false, List<string> propertiesToIgnore = null, List<Type> typesToIgnore = null)
+        {
+            IgnoreNulls = ignoreNulls;
+            PropertiesToIgnore = propertiesToIgnore ?? new List<string>();
+            TypesToIgnore = typesToIgnore ?? new List<Type>();
+        }
+    }
+}
