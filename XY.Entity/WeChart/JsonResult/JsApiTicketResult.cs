022a764123a85429e68e818b15c640c75e237bab
yunchenglk <yunchenglk@163.com>
2016-03-11 00:22:06 +0800

    1

---
+++ XY.Entity/WeChart/JsonResult/JsApiTicketResult.cs
@@ -0,0 +1,17 @@
+﻿namespace XY.Entity.Weixin
+{
+    /// <summary>
+    /// jsapi_ticket请求后的JSON返回格式
+    /// </summary>
+    public class JsApiTicketResult
+    {
+        /// <summary>
+        /// 获取到的凭证
+        /// </summary>
+        public string ticket { get; set; }
+        /// <summary>
+        /// 凭证有效时间，单位：秒
+        /// </summary>
+        public int expires_in { get; set; }
+    }
+}
---
+++ XY.Entity/WeChart/JsonResult/UploadMediaFileResult.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace XY.Entity.Weixin
+{
+    /// <summary>
+    /// 上传媒体文件返回结果
+    /// </summary>
+    public class UploadMediaFileResult
+    {
+        public UploadMediaFileType type { get; set; }
+        public string media_id { get; set; }
+        public long created_at { get; set; }
+    }
+}
