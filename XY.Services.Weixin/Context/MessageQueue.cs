de9bb208937f08880d21743564fbdc25c619b32b
yunchenglk <yunchenglk@163.com>
2016-03-11 00:30:06 +0800

    1

---
+++ XY.Services.Weixin/Context/MessageQueue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using XY.Services.Weixin.Entities;
+
+namespace XY.Services.Weixin.Context
+{
+    /// <summary>
+    /// 微信消息列队（针对单个账号的往来消息）
+    /// </summary>
+    /// <typeparam name="TM"></typeparam>
+    public class MessageQueue<TM, TRequest, TResponse> : List<TM>
+        where TM : class, IMessageContext<TRequest, TResponse>, new()
+        where TRequest : IRequestMessageBase
+        where TResponse : IResponseMessageBase
+    {
+
+    }
+}
