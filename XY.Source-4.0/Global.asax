05ed806e684726624fc09e3cc538a0bfadfa2060
Administrator <Administrator@DESKTOP-619CRSJ>
2016-09-11 21:39:36 +0800

    1

---
+++ XY.Source-4.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XY.Source_4._0.Global" Language="C#" %>
---
+++ XY.Source-4.0/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace XY.Source_4._0
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // 路由名称
+                "{controller}/{action}/{id}", // 带有参数的 URL
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }, // 参数默认值
+                new string[] { "Html.Controllers" }
+            );
+        }
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
