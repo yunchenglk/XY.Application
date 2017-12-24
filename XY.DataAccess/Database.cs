abba803e9d2f6db895570a8d2ea335640e3d1bba
yunchenglk <yunchenglk@163.com>
2017-12-25 02:47:46 +0800

    1

--- XY.DataAccess/Database.cs
+++ XY.DataAccess/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,11 @@ namespace XY.DataAccess
         {
             get
             {
-                return new SqlServer(ConnectionConfig.Connections["ApplicationDB_ConnectionString"]);
+                var mongourl = ConfigurationManager.AppSettings["mongodb"];
+                if (string.IsNullOrEmpty(mongourl))
+                    return new SqlServer(ConnectionConfig.Connections["ApplicationDB_ConnectionString"]);
+                else
+                    return new SqlServer(ConnectionConfig.Connections["ApplicationDB_ConnectionString"], mongourl);
             }
         }
         public static SqlServer Test
