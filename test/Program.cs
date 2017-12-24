fae19c6cf255a56080dc40b310ebaddd06cf178f
yunchenglk <yunchenglk@163.com>
2017-12-24 23:29:33 +0800

    1

--- test/Program.cs
+++ test/Program.cs
@@ -14,23 +14,23 @@ namespace test
         static void Main(string[] args)
         {
 
-            var _db = new wx_userweixinService();
-            MongoServer mongodb = MongoServer.Create("mongodb://39.106.117.151:27017"); // 连接数据库
-            MongoDatabase mongoDataBase = mongodb.GetDatabase("0359idatabase"); // 选择数据库名
-            MongoCollection mongoCollection = mongoDataBase.GetCollection("wx_userweixin"); // 选择集合，相当于表
+            //var _db = new wx_userweixinService();
+            //MongoServer mongodb = MongoServer.Create("mongodb://39.106.117.151:27017"); // 连接数据库
+            //MongoDatabase mongoDataBase = mongodb.GetDatabase("0359idatabase"); // 选择数据库名
+            //MongoCollection mongoCollection = mongoDataBase.GetCollection("wx_userweixin"); // 选择集合，相当于表
 
-            var i = 0;
-            _db.GetEnum().ToList().ForEach(m =>
-            {
-                string json = JsonHelper.SerializeObject(m);
-                var document = BsonDocument.Parse(json);
-                document.Add("_id", new ObjectId(m.ID.ToString().Replace("-", "").Substring(0, 24)));
-                mongoCollection.Save(document);
-                i++;
-                Console.WriteLine(i + "：" + m.ID.ToString().Replace("-", "").Substring(0, 24));
-            });
-            Console.WriteLine("处理完毕");
-            Console.Read();
+            //var i = 0;
+            //_db.GetEnum().ToList().ForEach(m =>
+            //{
+            //    string json = JsonHelper.SerializeObject(m);
+            //    var document = BsonDocument.Parse(json);
+            //    document.Add("_id", new ObjectId(m.ID.ToString().Replace("-", "").Substring(0, 24)));
+            //    mongoCollection.Save(document);
+            //    i++;
+            //    Console.WriteLine(i + "：" + m.ID.ToString().Replace("-", "").Substring(0, 24));
+            //});
+            //Console.WriteLine("处理完毕");
+            //Console.Read();
         }
     }
 
